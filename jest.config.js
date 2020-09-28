c224e3fed2924e9b958019ad192d763fdaa6b069
theson <theson@styleshare.kr>
2020-09-28 10:16:44 +0900

    fix: 테스트 코드가 수행되지 않던 문제 수정

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+  roots: ['<rootDir>/test'],
+  testMatch: ['/**/*.(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    'context-injector': '<rootDir>/src/index',
+  },
+};
