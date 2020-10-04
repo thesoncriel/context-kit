96283b80fc6405fc20ae60586b894e1597732d9c
TheSON <theson@paran.com>
2020-10-04 12:19:36 +0900

    test: 윈도에서 테스트 시 테스트 파일을 못찾는 문제 수정

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/test'],
-  testMatch: ['/**/*.(spec|test).+(ts|tsx|js)'],
+  testMatch: ['**/*.(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
